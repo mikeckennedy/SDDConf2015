7bb196a4a71f99285e6f4e9679d6bab6e49ca7c0
Michael Kennedy <mikeckennedy+github@gmail.com>
2015-05-13 09:24:38 +0100

    starter code for mvc forms session.

---
+++ aspnet_forms/FormUsingWebApp/mvc_forms_starter/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace mvc_forms_starter.Controllers
+{
+	public class HomeController : BaseController
+	{
+		// Step 1: Make view strongly typed.
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
