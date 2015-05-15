d9e49538733defea57bb600d72661a1bbb35a9a5
Michael Kennedy <mikeckennedy+github@gmail.com>
2015-05-15 16:08:35 +0100

    csharp 1

---
+++ workshop/chsarp_intro/CarDeailer/CarDeailer/Mongo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Kennedy;
+
+namespace CarDeailer
+{
+	class Mongo : MongoDbDataContext
+	{
+		public Mongo() : base("WorkshopCars")
+		{
+		}
+
+		public IQueryable<Car> Cars
+		{
+			get { return this.GetCollection<Car>(); }
+		}
+
+		public MongoCollection<Car> CarsCollection
+		{
+			get { return this.GetMongoCollection<Car>(); }
+		}
+	}
+
+	[BsonIgnoreExtraElements]
+	public class Car : ISupportInitialize
+	{
+		public ObjectId Id { get; set; }
+
+		public string Model { get; set; }
+		public string Type { get; set; }
+		
+		[BsonIgnoreIfNull]
+		public Color Color { get; set; }
+		//public Owner Owner { get; set; }
+
+		public List<WorkHistory> History { get; set; }
+
+		//[BsonExtraElements]
+		//public BsonDocument AdditionalData { get; set; }
+
+		public Car()
+		{
+			this.History = new List<WorkHistory>();
+		}
+
+		public void BeginInit()
+		{
+		}
+
+		public void EndInit()
+		{
+			this.History.ForEach(h => h.Car = this);
+		}
+	}
+
+	public class SuperCar : Car
+	{
+		public int NumberOfTurbos { get; set; }
+	}
+
+	public class WorkHistory
+	{
+		public string Desc { get; set; }
+		public string Location { get; set; }
+		public double Price { get; set; }
+
+		[BsonIgnore]
+		public Car Car { get; set; }
+	}
+
+	public class Color
+	{
+		public string Name { get; set; }
+		public string SerialNumber { get; set; }
+
+		//[BsonExtraElements]
+		//public BsonDocument AdditionalData { get; set; }
+	}
+
+	public class Owner
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ workshop/chsarp_intro/CarDeailer/CarDeailer/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Driver.Builders;
+
+namespace CarDeailer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//RefreshSchema();
+			//AddData();
+			
+			QueryData();
+			//AddHistory();
+			Projection();
+		}
+
+		private static void Projection()
+		{
+			var mongo = new Mongo();
+			var query = Query<Car>.Where(c => c.History.Any());
+			var data = mongo.CarsCollection
+				.Find(query)
+				.SetFields("Model")
+				.AsEnumerable()
+				.Select(c => new {Id = c.Id, Model=c.Model});
+			foreach (var c in data)
+			{
+				Console.WriteLine(c);
+			}
+		}
+
+		private static void RefreshSchema()
+		{
+			var mongo = new Mongo();
+			foreach (var car in mongo.Cars)
+			{
+				mongo.Save(car);
+			}
+		}
+
+		private static void AddHistory()
+		{
+			var mongo = new Mongo();
+			var tesla = mongo.Cars.Single(c => c.Type == "Tesla");
+			tesla.History.Add(new WorkHistory() { Desc = "Change eletric brushes", Location = "UK", Price = 72730 });
+			tesla.History.Add(new WorkHistory() { Desc = "Dust removal", Location = "UK", Price = 700 });
+			tesla.History.Add(new WorkHistory() { Car = tesla, Desc = "Polish", Location = "USA", Price = 10000 });
+
+			mongo.Save(tesla);
+		}
+
+		private static void QueryData()
+		{
+			var mongo = new Mongo();
+
+			Console.WriteLine("All cars");
+			var cars = mongo.Cars.OrderBy(c => c.Model).ToArray();
+			foreach (var car in cars)
+			{
+				Console.WriteLine(car.Model + " " + car.Type);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("Expensive work");
+			var expensiveWork =
+				from c in mongo.Cars
+				where c.History.Any(h => h.Price > 500)
+				select c;
+
+			Console.WriteLine("query is: " + expensiveWork.ToMongoQueryText());
+
+			foreach (var car in expensiveWork)
+			{
+				Console.WriteLine(car.Model + " " + car.Type);
+				Console.WriteLine("Work:");
+				foreach (var h in car.History)
+				{
+					Console.WriteLine("  * {0} for {1} (from {2})",
+						h.Desc, h.Price, h.Car.Model);
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("Some work");
+			var work =
+				(from c in mongo.Cars
+				where c.History.Any()
+				select c)
+				.AsEnumerable()
+				.SelectMany(c => c.History);
+
+			foreach (var h in work)
+			{
+				Console.WriteLine("  * {0} for {1}", h.Desc, h.Price);
+				Console.WriteLine();
+			}
+		}
+
+		private static void AddData()
+		{
+			var mongo = new Mongo();
+
+			var car = new Car();
+			car.Color = new Color() { Name = "Light Blue", SerialNumber = "o09345890358" };
+			car.Model = "Model S";
+			car.Type = "Tesla";
+
+			mongo.Save(car);
+
+			car = new Car();
+			car.Color = new Color() { Name = "Yellow", SerialNumber = "o3459834598" };
+			car.Model = "Esprit";
+			car.Type = "Lotus";
+
+			mongo.Save(car);
+
+			//var car = new SuperCar();
+			//car.Color = new Color() { Name = "Yellow", SerialNumber = "1" };
+			//car.NumberOfTurbos = 4;
+			//car.Model = "380";
+			//car.Type = "Ferrari";
+
+			//mongo.Save((Car)car);
+		}
+	}
+}
